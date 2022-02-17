3a4482d90609f0ba8227a8371b025d8cd222d2a2
Rustam <rustamotorbaev@gmail.com>
2022-02-17 23:26:38 +0600

    start bottom nav

---
+++ lib/pages/navpages/fav_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class FavoritePage extends StatelessWidget {
+  const FavoritePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Container(
+      child:const Center(child:  Text(
+        "Favorite pages"
+        ),)
+     
+    );
+  }
+}
+
+
