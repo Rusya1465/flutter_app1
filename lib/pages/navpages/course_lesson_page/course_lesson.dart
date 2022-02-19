ee6e87d22002eb9f5df4968bed8893801b64d61c
Ariet <nasirdinov.ari@gmail.com>
2022-02-19 19:21:09 +0600

    added course lesson page

---
+++ lib/pages/navpages/course_lesson_page/course_lesson.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CourseLesson extends StatefulWidget {
+  const CourseLesson({Key? key}) : super(key: key);
+
+  @override
+  State<CourseLesson> createState() => _CourseLessonState();
+}
+
+class _CourseLessonState extends State<CourseLesson> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
